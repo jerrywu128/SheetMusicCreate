1f913cc5a048aea8788f500f0cafcbdce6c0ae85
jerry wu <a0938489128@gmail.com>
2020-11-02 10:45:48 +0800

    set all piano key test.

--- src/musicstaffcreate/msMidiDevice.java
+++ src/musicstaffcreate/msMidiDevice.java
@@ -6,8 +6,8 @@ import javax.swing.*;
 
 public class msMidiDevice extends JLayeredPane {
     MainWin parent;
-    JButton  b[],bb ;
-
+    JButton  b[],bb[] ;
+    int j=145;
 
     msMidiDevice(MainWin p){
 
@@ -15,26 +15,28 @@ public class msMidiDevice extends JLayeredPane {
         this.setBackground(Color.gray);
         this.setOpaque(true);
         this.setPreferredSize(new Dimension(0,250));
-        b = new JButton[15];
-
-        for(int i =0;i<15;i++) {
+        b = new JButton[14];
+        bb = new JButton[10];
+        for(int i =0;i<14;i++) {
 
             b[i] = new JButton();
 
             b[i].setBounds(95+i*68,30,70,220);
-            b[i].setBackground(Color.black);
-            b[i].setOpaque(true);
+            //b[i].setBackground(Color.white);
+            //b[i].setOpaque(true);
             this.add(b[i],DEFAULT_LAYER);
 
-            if(i==1) {
-                bb = new JButton();
+            if(i<10) {
 
-                bb.setBounds(140  , 30, 50, 180);
-                bb.setBackground(Color.black);
-                bb.setOpaque(true);
-                bb.setBorderPainted(false);
+                bb[i] = new JButton();
 
-                this.add(bb,PALETTE_LAYER);
+                if((i==2)||(i==5)||(i==7)) j += 70;
+                bb[i].setBounds(j+i*67  , 30, 40, 180);
+                bb[i].setBackground(Color.black);
+                bb[i].setOpaque(true);
+                bb[i].setBorderPainted(false);
+
+                this.add(bb[i],PALETTE_LAYER);
             }
 
         }
